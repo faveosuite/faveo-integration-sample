4cf75e4c048f921df95cc56054a30422b4329126
sadhasivam <sadhasivam.sekar@ladybirdweb.com>
2024-09-09 03:23:09 +0530

    small change

--- resources/views/layout/updateCallingFunction.blade.php
+++ resources/views/layout/updateCallingFunction.blade.php
@@ -13,7 +13,7 @@
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Configuring Settings</li>
+                            <li class="breadcrumb-item active">Update Calling Function</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
