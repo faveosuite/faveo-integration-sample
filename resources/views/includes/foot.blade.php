dbc8399e52d206344bbcaf8acb6d3beb6b2cbea3
sadhasivam <sadhasivam.sekar@ladybirdweb.com>
2024-10-07 14:12:04 +0530

    fc-1

--- resources/views/includes/foot.blade.php
+++ resources/views/includes/foot.blade.php
@@ -7,3 +7,37 @@
 <!-- AdminLTE App -->
 <script src={{ asset("themes/js/adminlte.min.js") }}></script>
 
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        // Get current URL
+        var currentUrl = window.location.href.replace(/\/+$/, '');
+
+        // Loop through all links
+        document.querySelectorAll('.nav-link').forEach(function(link) {
+            var linkHref = link.href.replace(/\/+$/, '');
+
+            if (linkHref === currentUrl) {
+                // Add 'active' class to the current link
+                link.classList.add('active');
+
+                // Find the closest parent '.nav-item'
+                var navItem = link.closest('.nav-item');
+
+                // Check if the current link is within a treeview
+                if (navItem && navItem.closest('.nav-treeview')) {
+                    // Open its parent menu
+                    var parentNavItem = navItem.closest('.nav-treeview').parentElement;
+                    parentNavItem.classList.add('menu-open');
+
+                    // Optionally add 'active' class to the parent link if you want to indicate that a submenu is open
+                    var parentLink = parentNavItem.querySelector('.nav-link');
+                    if (parentLink) {
+                        parentLink.classList.add('active');
+                    }
+                }
+            }
+        });
+    });
+
+
+</script>
